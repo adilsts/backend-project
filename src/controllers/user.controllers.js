e90bd189b21057478b4f45c7ae4f15b94d89afb7
adilsts <adil.strongtechsolution@gmail.com>
2024-05-27 15:11:22 +0500

    fix bugs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -179,7 +179,7 @@ const logoutUser = asynchandlers(async (req, res) => {
 const refreshAccessToken = asynchandlers (async (req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if(incomingRefreshToken){
+    if(!incomingRefreshToken){
         throw new ApiError(401, "unathorized request")
 
     }
